606753bcddfed6f28227313a828aab80fbccf896
Azizul Hakim <azizulhakimgps@gmail.com>
2023-12-02 15:58:14 +0600

    Added Profile Page with Static data from data.jsx

---
+++ data.jsx
@@ -0,0 +1,30 @@
+export const userData = [{
+    id:1,
+    name: "Azizul Hakim",
+    image:"/my.png",
+    profession:"Software Developer",
+    resume: "/Azizul_Hakim_Resume_ca.pdf",
+    phone:"01624704422",
+    email:"azizulhakimgps@gmail.com",
+    skills:[
+        {name:'HTML',level:95},
+        {name:'CSS',level:85},
+        {name:'JavaScript',level:90},
+        {name:'React',level:90},
+        {name:'Redux',level:90}
+    ],
+    about:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus est vitae tortor ullamcorper, ut vestibulum velit convallis. Aenean posuere risus non velit egestas suscipit. Nunc finibus vel ante id euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Nulla vulputate pharetra tellus, in luctus risus rhoncus id.",
+    social:[
+        {name:"LinkedIn", link:"https://linkedin.com"},
+        {name:"Facebook", link:"https://facebook.com"},
+        {name:"Youtube", link:"https://youtube.com"},
+        {name:"instagram", link:"https://instagram.com"},
+        {name:"twitter", link:"https://twitter.com"}
+    ],
+    experience:[
+        {title:"Web Developer",companyName:"ABC Company",time:"2017-2019", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus est vitae tortor ullamcorper, ut vestibulum velit convallis. Aenean posuere risus non velit egestas suscipit."},
+        {title:"Computer Operator",companyName:"Hudai Company",time:"2019-2023", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus est vitae tortor ullamcorper, ut vestibulum velit convallis. Aenean posuere risus non velit egestas suscipit."},
+        {title:"Web Developer",companyName:"ABC Company",time:"2017-2019", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus est vitae tortor ullamcorper, ut vestibulum velit convallis. Aenean posuere risus non velit egestas suscipit."},
+        {title:"Computer Operator",companyName:"Hudai Company",time:"2019-2023", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus est vitae tortor ullamcorper, ut vestibulum velit convallis. Aenean posuere risus non velit egestas suscipit."},
+    ]
+}]
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,157 +1,108 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+import { Link } from "react-router-dom";
+import { userData } from "../../data";
 
 const Profile = () => {
+  const [showContact, setShowContact] = useState(false);
+  const user = userData[0];
   return (
     <Layout>
-        <div className="bg-gradient-to-r from-teal-300 to-teal-700">
-            <div className="h-[80vh] w-[1152px] mx-auto justify-between items-center grid grid-cols-3">
-              <div className="">
-                <img src="/my.png" alt="" />
+      <div className="bg-gray-100 h-[80vh]">
+        <div className="container mx-auto py-8">
+          <div className="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4">
+            <div className="col-span-4 sm:col-span-3">
+              <div className="bg-white shadow rounded-lg p-6">
+                <div className="flex flex-col items-center">
+                  <img
+                    src={user.image}
+                    className="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0"
+                  />
+                  <h1 className="text-xl font-bold">{user.name}</h1>
+                  <p className="text-gray-600">{user.profession}</p>
+                  <div className="mt-6 flex flex-wrap gap-4 justify-center">
+                    <button
+                      href="#"
+                      className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+                      onClick={() => setShowContact(!showContact)}
+                    >
+                      Contact
+                    </button>
+                    <Link
+                      to={"/"}
+                      className="bg-gray-300 hover:bg-gray-400 text-gray-700 py-2 px-4 rounded"
+                    >
+                      Resume
+                    </Link>
+                  </div>
+                  <div
+                    className={`text-gray-600 mt-6 ${
+                      showContact ? "" : "hidden"
+                    }`}
+                  >
+                    <p>Phone: {user.phone}</p>
+                    <p>{user.email}</p>
+                  </div>
+                </div>
+                <hr className="my-6 border-t border-gray-300" />
+                <div className="flex flex-col">
+                  <span className="text-gray-600 uppercase font-bold tracking-wider mb-2">
+                    Skills
+                  </span>
+                  <ul>
+                    {user.skills.map((skill) => {
+                      return (
+                        <div className="flex justify-between">
+                          <li className="mb-2">{skill.name}</li>
+                          <p>{skill.level}</p>
+                        </div>
+                      );
+                    })}
+                  </ul>
+                </div>
               </div>
-              
+            </div>
+            <div className="col-span-4 sm:col-span-9">
+              <div className="bg-white shadow rounded-lg p-6">
+                <h2 className="text-xl font-bold mb-4">About Me</h2>
+                <p className="text-gray-700">{user.about}</p>
+                <h3 className="font-semibold text-center mt-3 -mb-2">
+                  Find me on
+                </h3>
+                <div className="flex justify-center items-center gap-6 my-6">
+                  {user.social.map((links) => {
+                    return <Link to={links.link}>{links.name}</Link>;
+                  })}
+                </div>
+                <h2 className="text-xl font-bold mt-6 mb-4">Experience</h2>
 
-
-<div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-  <div className="sm:hidden">
-    <label htmlFor="tabs" className="sr-only">Select tab</label>
-    <select id="tabs" className="bg-gray-50 border-0 border-b border-gray-200 text-gray-900 text-sm rounded-t-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-      <option>Statistics</option>
-      <option>Services</option>
-      <option>FAQ</option>
-    </select>
-  </div>
-  <ul className="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg sm:flex dark:divide-gray-600 dark:text-gray-400 rtl:divide-x-reverse" id="fullWidthTab" data-tabs-toggle="#fullWidthTabContent" role="tablist">
-    <li className="w-full">
-      <button id="stats-tab" data-tabs-target="#stats" type="button" role="tab" aria-controls="stats" aria-selected="true" className="inline-block w-full p-4 rounded-ss-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Statistics</button>
-    </li>
-    <li className="w-full">
-      <button id="about-tab" data-tabs-target="#about" type="button" role="tab" aria-controls="about" aria-selected="false" className="inline-block w-full p-4 bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Services</button>
-    </li>
-    <li className="w-full">
-      <button id="faq-tab" data-tabs-target="#faq" type="button" role="tab" aria-controls="faq" aria-selected="false" className="inline-block w-full p-4 rounded-se-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">FAQ</button>
-    </li>
-  </ul>
-  <div id="fullWidthTabContent" className="border-t border-gray-200 dark:border-gray-600">
-    <div className="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="stats" role="tabpanel" aria-labelledby="stats-tab">
-      <dl className="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-6 dark:text-white sm:p-8">
-        <div className="flex flex-col items-center justify-center">
-          <dt className="mb-2 text-3xl font-extrabold">73M+</dt>
-          <dd className="text-gray-500 dark:text-gray-400">Developers</dd>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <dt className="mb-2 text-3xl font-extrabold">100M+</dt>
-          <dd className="text-gray-500 dark:text-gray-400">Public repositories</dd>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <dt className="mb-2 text-3xl font-extrabold">1000s</dt>
-          <dd className="text-gray-500 dark:text-gray-400">Open source projects</dd>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <dt className="mb-2 text-3xl font-extrabold">1B+</dt>
-          <dd className="text-gray-500 dark:text-gray-400">Contributors</dd>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <dt className="mb-2 text-3xl font-extrabold">90+</dt>
-          <dd className="text-gray-500 dark:text-gray-400">Top Forbes companies</dd>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <dt className="mb-2 text-3xl font-extrabold">4M+</dt>
-          <dd className="text-gray-500 dark:text-gray-400">Organizations</dd>
-        </div>
-      </dl>
-    </div>
-    <div className="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="about" role="tabpanel" aria-labelledby="about-tab">
-      <h2 className="mb-5 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-white">We invest in the world’s potential</h2>
-      {/* List */}
-      <ul role="list" className="space-y-4 text-gray-500 dark:text-gray-400">
-        <li className="flex space-x-2 rtl:space-x-reverse items-center">
-          <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span className="leading-tight">Dynamic reports and dashboards</span>
-        </li>
-        <li className="flex space-x-2 rtl:space-x-reverse items-center">
-          <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span className="leading-tight">Templates for everyone</span>
-        </li>
-        <li className="flex space-x-2 rtl:space-x-reverse items-center">
-          <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span className="leading-tight">Development workflow</span>
-        </li>
-        <li className="flex space-x-2 rtl:space-x-reverse items-center">
-          <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span className="leading-tight">Limitless business automation</span>
-        </li>
-      </ul>
-    </div>
-    <div className="hidden p-4 bg-white rounded-lg dark:bg-gray-800" id="faq" role="tabpanel" aria-labelledby="faq-tab">
-      <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-800 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
-        <h2 id="accordion-flush-heading-1">
-          <button type="button" className="flex items-center justify-between w-full py-5 font-medium text-left rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" data-accordion-target="#accordion-flush-body-1" aria-expanded="true" aria-controls="accordion-flush-body-1">
-            <span>What is Flowbite?</span>
-            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5 5 1 1 5" />
-            </svg>
-          </button>
-        </h2>
-        <div id="accordion-flush-body-1" className="hidden" aria-labelledby="accordion-flush-heading-1">
-          <div className="py-5 border-b border-gray-200 dark:border-gray-700">
-            <p className="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
-            <p className="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" className="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
-          </div>
-        </div>
-        <h2 id="accordion-flush-heading-2">
-          <button type="button" className="flex items-center justify-between w-full py-5 font-medium text-left rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" data-accordion-target="#accordion-flush-body-2" aria-expanded="false" aria-controls="accordion-flush-body-2">
-            <span>Is there a Figma file available?</span>
-            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5 5 1 1 5" />
-            </svg>
-          </button>
-        </h2>
-        <div id="accordion-flush-body-2" className="hidden" aria-labelledby="accordion-flush-heading-2">
-          <div className="py-5 border-b border-gray-200 dark:border-gray-700">
-            <p className="mb-2 text-gray-500 dark:text-gray-400">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
-            <p className="text-gray-500 dark:text-gray-400">Check out the <a href="https://flowbite.com/figma/" className="text-blue-600 dark:text-blue-500 hover:underline">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
-          </div>
-        </div>
-        <h2 id="accordion-flush-heading-3">
-          <button type="button" className="flex items-center justify-between w-full py-5 font-medium text-left rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" data-accordion-target="#accordion-flush-body-3" aria-expanded="false" aria-controls="accordion-flush-body-3">
-            <span>What are the differences between Flowbite and Tailwind UI?</span>
-            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5 5 1 1 5" />
-            </svg>
-          </button>
-        </h2>
-        <div id="accordion-flush-body-3" className="hidden" aria-labelledby="accordion-flush-heading-3">
-          <div className="py-5 border-b border-gray-200 dark:border-gray-700">
-            <p className="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-            <ul className="ps-5 text-gray-500 list-disc dark:text-gray-400">
-              <li><a href="https://flowbite.com/pro/" className="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
-              <li><a href="https://tailwindui.com/" rel="nofollow" className="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
-            </ul>
+                {user.experience.map((exp) => {
+                  return (
+                    <div className="mb-6">
+                      <div className="flex justify-between">
+                        <span className="text-gray-600 font-bold">
+                          {exp.title}
+                        </span>
+                        <p>
+                          <span className="text-gray-600 mr-2">
+                            at {exp.companyName}
+                          </span>
+                          <span className="text-gray-600">{exp.time}</span>
+                        </p>
+                      </div>
+                      <p className="mt-2">
+                        {exp.desc}
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
-
-
-
-              <div className=""></div>
-            </div>
-        </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
